f015061d1ce9666048250a652e63f609d81815c4
Carlos Arbelaez <caarbelaezg@gmail.com>
2021-02-23 14:03:39 -0500

    layout build

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="Footer">
+      <p className="Footer-title">Platzi Conf Merch</p>
+      <p className="Footer-copy">Todos los izquierdos Reservados</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className="Header">
+      <h1 className="Header-title"><Link to="/">PlatziConf Merch</Link></h1>
+      <div className="Header-checkout">
+        <Link to="/checkout"><i className="fas fa-shopping-basket"/></Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
